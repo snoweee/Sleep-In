e0aeb05ffaaa31b02c50d12c7984418386a07381
Dragomir Adrian <dragomir.adrian.catalin@gmail.com>
2016-05-13 20:41:15 +0300

    Add files via upload

---
+++ SleepForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿using System.Windows.Forms;
+
+namespace Sleep_In
+{
+    partial class SleepForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hours = new System.Windows.Forms.NumericUpDown();
+            this.minutes = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hours)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(199, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 128);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Functions";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 44);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(105, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Do Volume Mute";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(91, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Do Shutdown";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start Timer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Timer ";
+            // 
+            // hours
+            // 
+            this.hours.Location = new System.Drawing.Point(55, 35);
+            this.hours.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.hours.Name = "hours";
+            this.hours.Size = new System.Drawing.Size(40, 20);
+            this.hours.TabIndex = 6;
+            // 
+            // minutes
+            // 
+            this.minutes.Location = new System.Drawing.Point(115, 35);
+            this.minutes.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.minutes.Name = "minutes";
+            this.minutes.Size = new System.Drawing.Size(36, 20);
+            this.minutes.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = ":";
+            // 
+            // SleepForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 152);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hours);
+            this.Controls.Add(this.minutes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "SleepForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sleep In";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.hours)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private NumericUpDown hours;
+        private NumericUpDown minutes;
+        private Label label2;
+    }
+}
+
---
+++ SleepForm.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Management;
+using System.Diagnostics;
+
+namespace Sleep_In
+{
+    public partial class SleepForm : Form
+    {
+           
+
+        Thread timerThread;
+
+        
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern int ExitWindowsEx(int flag, int reserved);
+
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        static volatile bool keepRunning = false;
+        public SleepForm()
+        {
+            InitializeComponent();
+            minutes.Value = 10;
+            checkBox2.Checked = true;
+        }
+
+        private void Shutdown()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    Shutdown();
+                }));
+            }
+            else
+            {
+                var psi = new ProcessStartInfo("shutdown", "/s /t 0");
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                Process.Start(psi);
+            }
+        }
+        private void Mute()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    Mute();
+                }));
+            }
+            else
+            {
+                SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                    (IntPtr)APPCOMMAND_VOLUME_MUTE);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(timerThread ==null || !timerThread.IsAlive)
+            {
+                keepRunning = true;
+                timerThread = new Thread(new ThreadStart(timerThreadFunction));
+                timerThread.Start();
+            }
+            else if(timerThread.IsAlive)
+            {
+                keepRunning = false;
+                timerThread.Abort();
+                enableControls();
+            }
+        }
+
+        private void disableControls()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    disableControls();
+                }));
+            }
+            else
+            {
+                button1.Text = "Stop";
+                hours.Enabled = false;
+                minutes.Enabled = false;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+            }
+        }
+
+        private void enableControls()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    enableControls();
+                }));
+            }
+            else
+            {
+                button1.Text = "Start Timer";
+                hours.Enabled = true;
+                minutes.Enabled = true;
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+            }
+        }
+
+        private void showTimer(int totalTime)
+        {
+            if(this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    showTimer(totalTime);
+                }));
+            }
+            else
+            {
+                int hours_value = totalTime / 3600;
+                totalTime -= hours_value * 3600;
+                int minutes_value = totalTime / 60;
+
+                hours.Value = hours_value;
+                minutes.Value = minutes_value;
+            }
+        }
+
+        private void timerThreadFunction()
+        {
+            disableControls();
+
+            int hours_to_sec = (int)hours.Value * 3600;
+            int minutes_to_sec = (int)minutes.Value * 60;
+
+            int totalTime = hours_to_sec + minutes_to_sec;
+
+            while(keepRunning)
+            {
+                if (totalTime > 0)
+                    totalTime--;
+                else
+                {
+                    keepRunning = false;
+                    if(checkBox1.Checked)
+                    {
+                        this.Shutdown();
+                    }
+                    if(checkBox2.Checked && !checkBox1.Checked)
+                    {
+                        this.Mute();
+                    }
+                }
+                showTimer(totalTime);
+                Thread.Sleep(1000);
+            }
+                enableControls();
+        }
+    }
+}
